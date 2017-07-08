b79405ac04a624201d6a38eedfd658e462726a74
Binh-Phi Nguyen <kugoo109@gmail.com>
2017-07-09 00:54:14 +0200

    add solution for the required questions

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -17,12 +17,10 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
-
-
+    prediction = X * theta;
+    theta(1) = theta(1) - alpha / m * sum((prediction - y) .* X(:,1))
+    theta(2) = theta(2) - alpha / m * sum((prediction - y) .* X(:,2))
+    
     % ============================================================
 
     % Save the cost J in every iteration    
