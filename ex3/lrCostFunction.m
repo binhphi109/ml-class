682289771a09160e96fce75e0dfc273e52ae309f
Binh-Phi Nguyen <kugoo109@gmail.com>
2017-07-14 11:38:16 +0200

    vectorizing Logistic Regression

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -36,14 +36,11 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
-
-
-
-
-
-
-
+h = sigmoid(X * theta);
+J = 1 / m * sum(-y .* log(h) - (1 - y) .* log(1 - h)) + lambda / (2 * m) * sum(theta(2:end) .^ 2);
+temp = theta;
+temp(1) = 0;
+grad = 1 / m * (X' * (h - y) + lambda / m * temp);
 
 % =============================================================
 
